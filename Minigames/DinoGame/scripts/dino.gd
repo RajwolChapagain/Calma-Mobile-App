54f512165f21bbad5166a927ca647403400ee268
hiraniziyan <141653186+hiraniziyan@users.noreply.github.com>
2025-11-23 15:22:05 -0500

    Add files via upload

---
+++ Minigames/DinoGame/scripts/dino.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+const GRAVITY := 2000.0
+const JUMP_SPEED := -900.0
+const GROUND_Y := 260.0
+
+var velocity_y := 0.0
+var frozen := false
+signal died
+
+func _ready() -> void:
+	position = Vector2(0, 0)
+	visible = true
+	
+
+func _physics_process(delta: float) -> void:
+	if frozen:
+		return
+
+	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up"):
+		if _is_on_ground():
+			velocity_y = JUMP_SPEED
+
+	velocity_y += GRAVITY * delta
+	position.y += velocity_y * delta
+
+	if position.y > GROUND_Y:
+		position.y = GROUND_Y
+		velocity_y = 0.0
+
+	queue_redraw()
+
+func _is_on_ground() -> bool:
+	return abs(position.y - GROUND_Y) < 0.5
+
+func _draw() -> void:
+	draw_rect(Rect2(Vector2(-50, -50), Vector2(100, 100)), Color(1, 0, 0))
+
+
+func die() -> void:
+	emit_signal("died")
