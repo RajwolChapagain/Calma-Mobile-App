6159135f295590f4b26f898f482e2ba577957e7f
StopNWatch <vismith23@outlook.com>
2025-11-23 15:40:07 -0500

    Shop Item Progress

---
+++ Shop/Scenes/shop_item.gd
@@ -0,0 +1,10 @@
+extends Button
+class_name ShopItem
+
+@export var cost:int = 1
+@export var incon: Texture2D
+@export var boucht: bool = false
+@export var item_name: String = "Unnamed"
+@export var equiped: bool = false
+
+#Pressing button only brings up tool tip
