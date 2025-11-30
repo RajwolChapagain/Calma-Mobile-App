ee08aa61ace39e30f708fa84a34ab473c4620aea
StopNWatch <vismith23@outlook.com>
2025-11-30 15:09:34 -0500

    Shop Item tool tip edits

--- Shop/Scenes/shop_item.gd
+++ Shop/Scenes/shop_item.gd
@@ -2,9 +2,14 @@ extends Button
 class_name ShopItem
 
 @export var cost:int = 1
-@export var incon: Texture2D
-@export var boucht: bool = false
+#@export var incon: Texture2D
+@export var bought: bool = false
 @export var item_name: String = "Unnamed"
 @export var equiped: bool = false
+
+@export var desc: String = "No Description":
+	set(value):
+		$CanvasLayer/CenterContainer/ToolTip/MarginContainer/Description.text = value
+		desc = value
 
 #Pressing button only brings up tool tip
