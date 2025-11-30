b61c1dd4f6a243a4fb2c6deb29b2cdcac88134b0
StopNWatch <vismith23@outlook.com>
2025-11-30 16:59:56 -0500

    Shop panel/icon progress/tool tip functionality

--- Shop/Scenes/shop_item.gd
+++ Shop/Scenes/shop_item.gd
@@ -1,15 +1,40 @@
 extends Button
 class_name ShopItem
 
+
+@export var toolTipIcon: TextureRect
+@export var toolDescNode: RichTextLabel
+
 @export var cost:int = 1
-#@export var incon: Texture2D
+@export var shop_icon: Texture2D:
+	set(value):
+		icon = value
+		if is_instance_valid(toolTipIcon):
+			toolTipIcon.texture = value
+		shop_icon = value
 @export var bought: bool = false
 @export var item_name: String = "Unnamed"
 @export var equiped: bool = false
 
+
+
 @export var desc: String = "No Description":
 	set(value):
-		$CanvasLayer/CenterContainer/ToolTip/MarginContainer/Description.text = value
+		if is_instance_valid(toolDescNode):
+			toolDescNode.text = value
 		desc = value
 
+func _ready():
+	toolTipIcon.texture = shop_icon
+	icon = shop_icon
+	toolDescNode.text = desc
 #Pressing button only brings up tool tip
+
+
+
+func _on_button_down():
+	#emit some signal for button down to hide the other tool tips
+	$CanvasLayer.visible = true
+
+func _on_close_button_button_down():
+	$CanvasLayer.visible = false
