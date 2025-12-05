2ac37c4ef8df4d412c9cc9a6a7d76c5015e4d773
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-12-05 10:02:21 -0500

    Added SavedItems resource

--- Global/Scripts/Utils.gd
+++ Global/Scripts/Utils.gd
@@ -1,3 +1,9 @@
 extends Node
 
-var coins:int = 5
+class SavedItems:
+	extends Resource
+	@export var coins: int = 5
+	@export var bought_guis: Array = []
+	@export var bought_avatars: Array = []
+	@export var active_gui: int = 0
+	@export var active_avatar: int = 0
